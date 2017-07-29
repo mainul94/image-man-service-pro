ff9d25a6b3d70824ad96cae2a829033409f26b5d
Mainul Islam <mainulkhan94@gmail.com>
2017-07-29 21:11:36 +0600

    Set Default Folder

--- image_processing_com/zenith/doctype/folder_manage/folder_manage.py
+++ image_processing_com/zenith/doctype/folder_manage/folder_manage.py
@@ -5,19 +5,20 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from frappe.utils import get_files_path
+from frappe.defaults import set_global_default
+
 
 class FolderManage(Document):
-	def validate(self):
-		pass
-		self.path = self.title
+    def validate(self):
+        self.path = self.title
 
-	def on_update(self):
-		if not frappe.db.exists("File", self.title):
-			frappe.get_doc({
-				"doctype": "File",
-				"is_folder": 1,
-				"is_home_folder": 1,
-				"file_name": self.title
-			}).insert()
-			# frappe.create_folder(get_files_path(self.title))
+    def on_update(self):
+        if self.is_default:
+            set_global_default(frappe.scrub(self.folder_type)+'_folder', self.name)
+        if not frappe.db.exists("File", self.title):
+            frappe.get_doc({
+                "doctype": "File",
+                "is_folder": 1,
+                "is_home_folder": 1,
+                "file_name": self.title
+            }).insert()
