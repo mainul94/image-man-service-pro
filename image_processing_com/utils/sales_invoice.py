6a140b9855466c738cd92689dee9f5d18280752b
Mainul Islam <mainulkhan94@gmail.com>
2017-09-24 13:43:55 +0600

    Fixed Folder Created on Sales INvoice.

--- image_processing_com/utils/sales_invoice.py
+++ image_processing_com/utils/sales_invoice.py
@@ -6,8 +6,10 @@ def submit_invoice(doc, method):
     folders = [doc.download_folder, doc.qc_folder, doc.upload_folder, doc.upload_backup_folder,
                doc.download_backup_folder, doc.output_folder]
     for folder in folders:
-        create_new_folder(doc.name, folder)
-        create_new_folder(doc.customer_job_no, folder + '/' + doc.name)
+        if not frappe.db.exists("File", folder+'/'+doc.name):
+            create_new_folder(doc.name, folder)
+        if not frappe.db.exists("File", folder + '/' + doc.name + '/' + doc.customer_job_no):
+            create_new_folder(doc.customer_job_no, folder + '/' + doc.name)
 
 
 def test_method(doc, method):
