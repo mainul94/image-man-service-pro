96ce8ea3b32a49355b0dcb25fd43bba3634cadca
Mainul Islam <mainulkhan94@gmail.com>
2017-11-05 14:10:20 +0600

    Complete Designer Log Report

---
+++ image_processing_com/zenith/report/designer_log/designer_log.js
@@ -0,0 +1,45 @@
+// Copyright (c) 2016, Mianul Islam and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Designer Log"] = {
+	"filters": [
+		{
+			fieldname: "job_no",
+			fieldtype: "Link",
+			options: "Sales Invoice",
+			label: __("Job No")
+		},
+		{
+			fieldname: 'from_date',
+			fieldtype: 'Date',
+			label: __("From"),
+			reqd: true,
+			default: frappe.datetime.month_start()
+		},
+		{
+			fieldname: 'to_date',
+			fieldtype: 'Date',
+			label: __("To"),
+			reqd: true,
+			default: frappe.datetime.month_end()
+		},
+		{
+			fieldname: 'employee',
+			fieldtype: 'Link',
+			options: 'Employee',
+			label: __("Designer")
+		},
+		{
+			fieldname: 'file',
+			fieldtype: 'Data',
+			label: __("File Name")
+		},
+		{
+			fieldname: 'status',
+			fieldtype: 'Select',
+            options: "\nAssign\nHold\nFinished\nRename\nBack File\nReturn\nWrong",
+			label: __("Status")
+		}
+	]
+};
