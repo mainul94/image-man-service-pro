5da312e40d7ed58638b8f89e1815a4620dec6170
Mainul Islam <mainulkhan94@gmail.com>
2018-03-24 11:23:32 +0600

    Added List view but that is not correct

--- image_processing_com/zenith/page/action_on_log/action_on_log.js
+++ image_processing_com/zenith/page/action_on_log/action_on_log.js
@@ -1,7 +1,19 @@
 frappe.pages['action-on-log'].on_page_load = function(wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'None',
+		title: 'Change Status',
 		single_column: true
 	});
+	page.doctype = page.add_select(__("Log"), ['Designer Log', 'QC Log'])
+	page.ListViewWrapper = $('<div>').appendTo(page.body)
+	$.extend(frappe.boot.user.all_read, ['Designer Log', 'QC Log'])
+	// page.doctype.on('change', function(e){
+	// 	frappe.model.with_doctype (page.doctype.val(), function (){
+	// 		wrapper.page.ListView = new frappe.views.ListView({
+	// 			page: page,
+	// 			parent: wrapper,
+	// 			doctype: page.doctype.val()
+	// 		});
+	// 	})
+	// })
 }
